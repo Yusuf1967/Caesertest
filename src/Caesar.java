9ba0cd01661165defc7295460b55f59a149988bf
oezdemyu <yusufoz61@gmx.de>
2024-11-08 10:38:00 +0100

    <caeser>, return geändert

--- src/Caesar.java
+++ src/Caesar.java
@@ -11,17 +11,17 @@ class Caesar {
     public void entschuessel() {}
 
     public int getASCII(char pWert) {
-        return
+        return (int) pWert;
     }
 
     public char getChar(int pWert) {
-        return
+        return (char) pWert;
     }
 
     public void getKt(String pWert) {}
 
     public String getKt() {
-        return
+        return (String) Kt;
     }
 
     public void getS(int pWert) {};
