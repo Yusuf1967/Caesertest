55f4e6b47515ace0bf567e2f62a0da3c5f83e650
oezdemyu <yusufoz61@gmx.de>
2024-11-08 10:29:39 +0100

    <caeser>, return geändert

---
+++ src/Caesar.java
@@ -0,0 +1,30 @@
+class Caesar {
+    private String Kt;
+    private int S;
+    private String gt;
+
+
+    public void casar() {};
+
+    public void verschuesseln(){};
+
+    public void entschuessel() {}
+
+    public int getASCII(char pWert) {
+        return
+    }
+
+    public char getChar(int pWert) {
+        return
+    }
+
+    public void getKt(String pWert) {}
+
+    public String getKt() {
+        return
+    }
+
+    public void getS(int pWert) {};
+
+
+}
