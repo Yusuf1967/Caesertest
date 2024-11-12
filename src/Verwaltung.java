804cca3d18fde49ebcf76cfa79ea0c9b5c2fbdfc
oezdemyu <yusufoz61@gmx.de>
2024-11-12 13:20:23 +0100

    <caeser>, return geändert

---
+++ src/Verwaltung.java
@@ -0,0 +1,27 @@
+
+public class Verwaltung {
+    private Scanner scanner;
+    private Caesar caesar;
+}
+
+    public static void main(String[] args) {new Verwaltung();}
+
+public Verwaltung(){
+    scanner = new Scanner(System.in)
+    while(true) {
+        System.out.println("==HAUPTMENÜ==");
+        System.out.println("[1] Caeser Verschluesseln");
+        System.out.println("[2] Caeser Entschluesseln");
+    }
+
+
+}
+
+    public Verwaltung() {
+        test = new Tester();
+        scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("== HAUPTMENÜ ==");
+            System.out.println("[1] Zeichenkette umdrehen");
+            System.out.println("[2] Palindromtest");
+            System.out.println("[0] Beenden");
