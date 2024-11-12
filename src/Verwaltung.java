2f31d10988011d1948427772e090484a0abf279d
Yusufs pc <yusufoz61@gmx.de>
2024-11-12 23:14:20 +0100

    ka

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -1,27 +1,43 @@
+import java.util.Scanner;
 
 public class Verwaltung {
-    private Scanner scanner;
-    private Caesar caesar;
+   private Scanner scanner;
+   scanner = new Scanner();
+   private Caeser caeser;
+   caeser = new Caeser();
 }
 
     public static void main(String[] args) {new Verwaltung();}
 
 public Verwaltung(){
-    scanner = new Scanner(System.in)
+    Scanner scanner = new Scanner(System.in);
     while(true) {
-        System.out.println("==HAUPTMENÜ==");
-        System.out.println("[1] Caeser Verschluesseln");
-        System.out.println("[2] Caeser Entschluesseln");
+        System.out.println("== HAUPTMENÜ ==");
+        System.out.println("[1] Caeser verschluesseln");
+        System.out.println("[2] Caeser entschluesseln");
+        int option = scanner.nextInt();
+        scanner.nextline();
+        if (option == 1){
+            System.out.println("Gib das zu verschlüsselnde wort an:");
+            String zeichenkette = scanner.nextLine();
+            System.out.println("Gib den Schlüssel an:");
+            String schluesselString = scanner.nextLine();
+            int schluessel = Integer.parseInt(schluesselString);
+            Caeser caeser = new Caeser();
+            this.caesar.setKt(zeichenkette);
+            this.caeser.setS(schluessel);
+            System.out.println("======Klartext======");
+            System.out.println(this.caeser.getKt());
+            this.caesar.verschluesseln();
+            System.out.println("======Geheimtext======");
+            System.out.println(this.caesar.getGt());
+
+        }
     }
 
 
 }
 
-    public Verwaltung() {
-        test = new Tester();
-        scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("== HAUPTMENÜ ==");
-            System.out.println("[1] Zeichenkette umdrehen");
-            System.out.println("[2] Palindromtest");
-            System.out.println("[0] Beenden");
+private final Caeser caesar;
+private final Object caeser;
+
