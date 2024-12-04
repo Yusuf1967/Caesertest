85ea235a9851e5c211bf8d16d07c7ed7353eeb09
oezdemyu <yusufoz61@gmx.de>
2024-12-04 11:20:56 +0100

    polybius

---
+++ src/Polybius.java
@@ -0,0 +1,20 @@
+public class Polybius extends Kryptomat{
+
+    private int [][] buchstabenArray;
+    protected void verschluesseln() {
+
+    }
+
+    protected void entschluessln() {
+
+    }
+
+
+    public void bfms(){
+
+    }
+    public Polybius(){
+    super();
+    this.buchstabenArray = new int [6][6];
+    }
+}
